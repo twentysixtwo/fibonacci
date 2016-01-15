4c7df355ebbe18d3c207f9f9c1025305f0ace297
Jonathan Huang <jonathan.huang@doublebridge.com>
2016-01-15 10:56:09 -0500

    Testing Git. Added input checking and more tests.

--- fibonacci/src/fibonacci.java
+++ fibonacci/src/fibonacci.java
@@ -11,13 +11,18 @@ public class fibonacci {
 	
 	public int check(int num) {
 		// works for cases num >= 0
-		switch(num) {
-		case 0:
+		if(num>0) {
+			switch(num) {
+			case 0:
+				return 0;
+			case 1:
+				return 1;
+			default:
+				return (check(num-1)+check(num-2));
+			}
+		} else {
+			//If num is negative - undefined. Return 0
 			return 0;
-		case 1:
-			return 1;
-		default:
-			return (check(num-1)+check(num-2));
 		}
 	}
 
--- fibonacci/src/fibonacciTest.java
+++ fibonacci/src/fibonacciTest.java
@@ -8,10 +8,12 @@ public class fibonacciTest {
 	@Test
 	public void test() {
 		fibonacci fib = new fibonacci();
-		
 		assertEquals("fibonacci 6 is 8", 8, fib.check(6));
 		assertEquals("fibonacci 12 is 144", 144, fib.check(12));
 		assertEquals("fibonacci 20 is 6765", 6765, fib.check(20));
+		assertEquals("fibonacci 1 is 1", 1, fib.check(1));
+		assertEquals("fibonacci 0 is 0", 0, fib.check(0));
+		assertEquals("fibonacci -1 is undefined. return 0", 0, fib.check(-1));
 	}
 
 }
